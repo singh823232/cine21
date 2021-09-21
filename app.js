6bdaa2e9e7caef77ff1f91a01a5ac949142a28ee
satyam singh <satyam1913196@akgec.ac.in>
2021-09-22 03:42:22 +0530

    update

--- app.js
+++ app.js
@@ -43,31 +43,31 @@ app.post("/register", async (req, res) => {
         const studentData = await condidateSchema.save()
         console.log(studentData);
 
-        //##################### using  nodemailer #################//
-        // const transporter = nodemailer.createTransport({
-        //     service: "gmail",
-        //     auth: {
-        //         user: process.env.EMAIL,
-        //         pass: process.env.PASS_E
-        //     }
-        // });
-        // const mailOption = {
-        //     from: process.env.EMAIL,
-        //     to: req.body.email,
-        //     subject: "CINE'21",    // https://www.youtube.com/redirect?event=video_description&redir_token=QUFFLUhqbGZVWUJFbnFPUFk1aGVkMm9UVHhnaWJjc1FYUXxBQ3Jtc0tuZnFNa3BMVkR5SkZIWm5lLWw0NEF5b0tsYnF3T2dnR3pXUm1xNVo4YTJ2cFJiLVJsdGpfc24zYkpaWGx5bnFiT3lLb0NGQk9zcVlqaF9lRXI3alN6Qm5XVXBHNEdNS2NkeERuY0tZaDg5ZW1aUkZEYw&q=https%3A%2F%2Fmyaccount.google.com%2Flesssecureapps
-        //     html: "<h3>CONGRATULATION,</h3><br>" + "<h1 style='font-weight:bold;'>You are successfully registered</h1>"
-        // };
+        // ##################### using  nodemailer #################//
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                user: process.env.EMAIL,
+                pass: process.env.PASS_E
+            }
+        });
+        const mailOption = {
+            from: process.env.EMAIL,
+            to: req.body.email,
+            subject: "CINE'21",    // https://www.youtube.com/redirect?event=video_description&redir_token=QUFFLUhqbGZVWUJFbnFPUFk1aGVkMm9UVHhnaWJjc1FYUXxBQ3Jtc0tuZnFNa3BMVkR5SkZIWm5lLWw0NEF5b0tsYnF3T2dnR3pXUm1xNVo4YTJ2cFJiLVJsdGpfc24zYkpaWGx5bnFiT3lLb0NGQk9zcVlqaF9lRXI3alN6Qm5XVXBHNEdNS2NkeERuY0tZaDg5ZW1aUkZEYw&q=https%3A%2F%2Fmyaccount.google.com%2Flesssecureapps
+            html: "<h3>CONGRATULATION,</h3><br>" + "<h1 style='font-weight:bold;'>You are successfully registered</h1>"
+        };
 
-        // transporter.sendMail(mailOption, function (error, info) {
-        //     if (error) {
-        //         console.log(error)
-        //         res.send(error);
-        //     }
-        //     else {
-        //         console.log(mailOption)
-        //         res.send(mailOption);
-        //     }
-        // });
+        transporter.sendMail(mailOption, function (error, info) {
+            if (error) {
+                console.log(error)
+                res.send(error);
+            }
+            else {
+                console.log(mailOption)
+                res.send(mailOption);
+            }
+        });
         res.status(200).send("You are successfully registered");
     }
     catch (error) {
